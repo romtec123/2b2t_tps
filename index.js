98e75f04ba67c197cbc43492a0bcd12a711c7d23
romtec123 <julianlevin11@gmail.com>
2020-11-10 12:21:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function checkLag(){
       	tps = parseFloat(tps)
       	if(tps <= 6){
       		if(isLagging == false){
-      			msgAll("2b2t-lag-test", "2B2T has started lagging. TPS: " + tps)
+      			msgAll("2b2t-lag", "2B2T has started lagging. TPS: " + tps)
             console.log("2B2T has started lagging. TPS: " + tps)
       			lagStartTime = Date.now()
       			minTps = tps
@@ -72,23 +72,23 @@ function checkLag(){
       			isLagging = false
       			tpsAvg = tpsAvg/tpsCount
       			let uptimesec = (Date.now() - lagStartTime) /  1000
-    			let hours = Math.floor(uptimesec / 60 / 60);
-    			let minutes = Math.floor(uptimesec / 60) - (hours * 60);
-    			let seconds = Math.floor(uptimesec % 60);
-    			//hours + 'h ' + minutes + 'm ' + seconds + 's.'
-    			let lagEmbed = new Discord.MessageEmbed()
-    			.setColor('#CC3333')
-    			.setTitle('2B2T Lag Information')
-    			.setURL('https://discord.gg/G2w7DQQ')
-    			.setAuthor('2B2BOT', 'https://i.imgur.com/mReItus.png', 'https://discord.gg/G2w7DQQ')
-    			.setDescription('Information on the recent lag spike on 2B2T.')
-    			.addFields(
+    			  let hours = Math.floor(uptimesec / 60 / 60);
+    			  let minutes = Math.floor(uptimesec / 60) - (hours * 60);
+    			  let seconds = Math.floor(uptimesec % 60);
+    			  //hours + 'h ' + minutes + 'm ' + seconds + 's.'
+    			  let lagEmbed = new Discord.MessageEmbed()
+    			  .setColor('#CC3333')
+    			  .setTitle('2B2T Lag Information')
+    			  .setURL('https://discord.gg/G2w7DQQ')
+    			  .setAuthor('2B2BOT', 'https://i.imgur.com/mReItus.png', 'https://discord.gg/G2w7DQQ')
+    			  .setDescription('Information on the recent lag spike on 2B2T.')
+    			  .addFields(
       				{ name: 'Average TPS during spike', value: tpsAvg },
      				  { name: 'Lag spike time', value: hours + 'h ' + minutes + 'm ' + seconds + 's.'},
       				{ name: 'Lowest recorded TPS', value: minTps },
       				{ name: 'Points', value: (totalPoints / 4)},
-    			)
-    			msgAll('2b2t-lag-test', lagEmbed);
+    			  )
+    			  msgAll('2b2t-lag', lagEmbed);
       		}
       	}
     })
